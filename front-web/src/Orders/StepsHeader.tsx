8bcb0b09d9f58c1bd8e9261e28505c2ed552eba3
unknown <semogfilho@gmail.com>
2021-01-25 01:14:59 -0300

    Search order location

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -4,19 +4,19 @@ function StepsHeader() {
     return (
         <header className="orders-steps-container">
             <div className="orders-steps-content">
-<h1 className="steps-title">
-SIGA AS <br /> ETAPAS
+                <h1 className="steps-title">
+                    SIGA AS <br /> ETAPAS
 </h1>
-<ul className="steps-items">
-<li>
-    <span className="steps-number">1</span>
+                <ul className="steps-items">
+                    <li>
+                        <span className="steps-number">1</span>
     Selecione os produtos e localização.
 </li>
-<li>
-    <span className="steps-number">2</span>
+                    <li>
+                        <span className="steps-number">2</span>
     Depois clique em <strong>"ENVIAR PEDIDO"</strong>
-</li>
-</ul>
+                    </li>
+                </ul>
             </div>
         </header>
     )
---
+++ front-web/src/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = 'https://semogfilho-sds2.herokuapp.com';
+const mapboxToken=process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
+
+export function fetchProducts() {
+    return axios(`${API_URL}/products`)
+}
+
+export function fectchLocalMapBox(local: string){
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
